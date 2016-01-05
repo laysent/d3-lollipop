a3b3d23bd22985a2e12583855e79c5a799498c14
Zhou YU <laysent@gmail.com>
2016-01-05 14:29:04 +0800

    ECMAScript 3

--- lollipop.js
+++ lollipop.js
@@ -44,10 +44,10 @@
       
       yLabelText: 'Visits',    // text for y axis
       
-      tooltipText: (d,i) => '',
+      tooltipText: function(d,i) { return ''; },
       tooltipDuration: 200,
 
-      parse: data => [data[0], [data[1], data[2]]],
+      parse: function(data) { return [data[0], [data[1], data[2]]]; },
     }
 
     var invisible = 1e-6, // JavaScript uses exponential expression for number smaller than 1e-6
@@ -80,7 +80,7 @@
         // returns the function that can be used to `transform` attribute
         // it will put element to the center of each bar
         var lollipopTransformHelper = function (yScaleFunc) {
-          return (d, i) => {
+          return function (d, i) {
             return 'translate(' + 
               (variable.barWidth / 2) + ',' +
               yScaleFunc(d) + ')';
@@ -112,10 +112,12 @@
         var arcTween = function (d) {
           var interpolate = d3.interpolate(this._current, d);
           this._current = interpolate(0);
-          return t => arc(interpolate(t));
+          return function(t) { return arc(interpolate(t)); };
         };
         // `sumPortion` is the sum of all values in portion 
-        var sumPortion = portion.reduce((previous, current) => previous + current, 0);
+        var sumPortion = portion.reduce(function(previous, current) { 
+            return previous + current; 
+            }, 0);
         // `pie` convert data to pie
         // depends on the size of `sumPortion`, the lollipop might not be
         // a complete circle
@@ -140,7 +142,7 @@
           .append('path')
           .attr({
             'class': 'lollipop',
-            'fill': (d, i) => variable.color[i],
+            'fill': function(d, i) { return variable.color[i]; },
             'd': arc
           })
           .style('opacity', invisible)
@@ -164,7 +166,7 @@
             'class': 'indicator',
             'x1': variable.barWidth / 2,
             'x2': variable.barWidth / 2,
-            'y1': d => previousYScale(value),
+            'y1': function(d) { return previousYScale(value); },
             'y2': previousYScale(0)
           }).
           style('visible', invisible);
@@ -173,7 +175,7 @@
           transition().
           duration(variable.duration).
           attr({
-            'y1': d => yScale(value) + variable.radius * 1.2,
+            'y1': function(d) { return yScale(value) + variable.radius * 1.2; },
             'y2': yScale(0)
           }).
           style('visible', visible);
@@ -188,7 +190,7 @@
             'class': 'center',
             'r': 2,
             'cx': variable.barWidth / 2,
-            'cy': d => previousYScale(d)
+            'cy': function(d) { return previousYScale(d); }
           }).
           style('opacity', invisible);
 
@@ -196,7 +198,7 @@
           transition().
           duration(variable.duration).
           style('opacity', visible).
-          attr('cy', d => yScale(d));
+          attr('cy', function(d) { return yScale(d); });
 
       }); // end of `selections.each()`
     }; // end of `drawSinglelollipopBar()`
@@ -214,7 +216,7 @@
 
     // y scale (stored in variable for further usage)
      variable.__yScale__ = d3.scale.linear()
-          .domain([0, d3.max(chartData, d => variable.parse(d)[0]) * 1.1])
+          .domain([0, d3.max(chartData, function(d) { return variable.parse(d)[0]; }) * 1.1])
           .range([ 
             variable.height - variable.radius - variable.bound.bottom,
             variable.bound.top
@@ -224,11 +226,11 @@
      // other conuts will only have partial circle, to reflect the sum
      // `__arcScale__` will calculate the end angle of each circle
      variable.__arcScale__ = d3.scale.linear()
-          .domain([0, d3.max(chartData, d => 
-            variable.parse(d)[1].reduce(
-              (previous, current) => previous + current, 
-              0)
-            )])
+          .domain([0, d3.max(chartData, function(d) { 
+            return variable.parse(d)[1].reduce(
+              function(previous, current) { return previous + current; }, 
+              0);
+          })])
           .range([0, 2 * Math.PI]);
 
      // create one DOM for tooltip
@@ -254,16 +256,16 @@
                     variable.bound.top - 
                     variable.bound.bottom,
           'width': variable.barWidth,
-          'transform': (d, i) => 'translate(' + 
+          'transform': function(d, i) { return 'translate(' + 
                     (i * variable.barWidth + 
-                      variable.bound.left) + ', 0)',
+                      variable.bound.left) + ', 0)'; },
           'class': 'bar'
         });
       // draw
       lollipops.call(drawSingleLollipop);
 
       // add events for show/hide tooltip
-      lollipops.on('mouseover', (d, i) => {
+      lollipops.on('mouseover', function(d, i) {
         variable.__tooltip__.style({
           'font-size': '10px',
           'margin-left': ((i + 1) * variable.barWidth + 
@@ -278,7 +280,7 @@
           'opacity': visible
           });
       });
-      lollipops.on('mouseout', (d, i) => {
+      lollipops.on('mouseout', function(d, i) {
         variable.__tooltip__
         .transition()
           .duration(variable.tooltipDuration)
@@ -326,7 +328,7 @@
           'class': 'legendRect',
           'height': legendBoxSize,
           'width': legendBoxSize,
-          'fill': (d, i) => variable.color[i],
+          'fill': function(d, i) { return variable.color[i]; },
           'transform': 'translate(' + 
             0 + ',' + 
             0 + ')'
@@ -339,7 +341,7 @@
             (legendBoxSize + 2) + 
             ', ' + (legendBoxSize) + ')'
         }).
-        text(d => d);
+        text(function(d) { return d; });
         newlegend.attr({
           'transform': function (d, i) {
               var box = this.getBBox(), 
@@ -356,7 +358,7 @@
       var xAxis = d3.svg.axis()
         .scale(ordinal)
         .orient('bottom')
-        .tickFormat(i => chartData[i]['name']);
+        .tickFormat(function(i) { return chartData[i]['name']; });
       var yAxis = d3.svg.axis()
         .scale(variable.__yScale__)
         .orient('left');
@@ -403,14 +405,14 @@
 
     // add configuration functions inside
     var customization = configHelper(lollipops, variable);
-    Object.keys(variable).forEach(keyword => {
+    Object.keys(variable).forEach(function(keyword) {
       lollipops[keyword] = customization(keyword);
     });
     lollipops.bound = function(top, right, bottom, left) {
         if (arguments.length == 0) return variable.bound;
         if (arguments.length == 1) {
             var config = top;
-            Object.keys(variable.bound).forEach(keyword => {
+            Object.keys(variable.bound).forEach(function(keyword) {
                 if (!config[keyword]) variable.bound[keyword] = config[keyword];
             });
         }
